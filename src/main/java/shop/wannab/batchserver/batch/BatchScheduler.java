34395f87e10fe17ee93928d8c1801b66cd50bc26
gnsals0904 <gnsals0904@naver.com>
2025-07-21 09:26:55 +0900

    feat: 외부 구성을 통해 스케쥴링 설정할 수 있도록 수정

--- src/main/java/shop/wannab/batchserver/batch/BatchScheduler.java
+++ src/main/java/shop/wannab/batchserver/batch/BatchScheduler.java
@@ -1,40 +1,55 @@
 package shop.wannab.batchserver.batch;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.stereotype.Component;
+import shop.wannab.batchserver.global.config.BatchScheduleProperties;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @EnableScheduling
-public class BatchScheduler {
+public class BatchScheduler implements SchedulingConfigurer {
 
     private final JobLauncher jobLauncher;
     private final Job updateUserGradeJob;
     private final Job updateUserStateJob;
+    private final BatchScheduleProperties scheduleProperties;
 
-    // TODO : 변경 필요
-    @Scheduled(cron = "0 */2 * * * ?")
-    public void runUpdateUserGradeJob() throws Exception {
+    public void runUpdateUserGradeJob() {
         JobParameters params = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
                 .toJobParameters();
+        try {
+            jobLauncher.run(updateUserGradeJob, params);
+        } catch (Exception e) {
+            log.error("runUpdateUserGradeJob Error : {}", e.getMessage());
+        }
 
-        jobLauncher.run(updateUserGradeJob, params);
     }
 
-    // TODO : 변경 필요
-    @Scheduled(cron = "0 1/2 * * * ?")
-    public void runUpdateUserStateJob() throws Exception {
+    public void runUpdateUserStateJob() {
         JobParameters params = new JobParametersBuilder()
                 .addLong("time", System.currentTimeMillis())
                 .toJobParameters();
+        try{
+            jobLauncher.run(updateUserStateJob, params);
+        } catch (Exception e){
+            log.error("runUpdateUserStateJob Error : {}", e.getMessage());
+        }
 
-        jobLauncher.run(updateUserStateJob, params);
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.addCronTask(this::runUpdateUserGradeJob, scheduleProperties.getUpdateUserGrade());
+        taskRegistrar.addCronTask(this::runUpdateUserStateJob, scheduleProperties.getUpdateUserState());
     }
 }
